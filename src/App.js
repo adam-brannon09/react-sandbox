d43f8bbc57f32395fe0b05d63c9005f54228f881
adam-brannon09 <adam.brannon09@gmail.com>
2023-10-04 13:23:32 -0500

    added callback example

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import UseRefExample1 from './components/UseRefExample1';
 import UseRefExample2 from './components/UseRefExample2';
 import UseRefExample3 from './components/UseRefExample3';
 import UseMemoExample from './components/UseMemoExample';
+import UseCallbackExample from './components/UseCallbackExample';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <UseRefExample1 /> */}
       {/* <UseRefExample2 /> */}
       {/* <UseRefExample3 /> */}
-      <UseMemoExample />
+      {/* <UseMemoExample /> */}
+      <UseCallbackExample />
     </div>
   );
 }
